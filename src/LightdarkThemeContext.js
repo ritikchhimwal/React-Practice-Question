8f06a4e95d1462b2ccde2482940fbb67c72298f5
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-03-11 19:29:13 +0530

    context for LightDark component

---
+++ src/LightdarkThemeContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({children}) => {
+    const [isDarkMode,setIsdarkMode] = useState(false);
+
+    const toggleTheme = ()=> {
+        setIsdarkMode((prevMode) => !prevMode)
+    };
+
+    return (
+        <ThemeContext.Provider value={{isDarkMode,toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+const useTheme = () =>{
+    return useContext(ThemeContext);
+}
+export {useTheme,ThemeProvider}
