0ad5a3e5ac381295ffbad4a5f22a268dcbdb4891
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-03-11 19:31:32 +0530

    Create Pagination

---
+++ src/Pagination.js
@@ -0,0 +1,32 @@
+//Develop a pagination component to navigate through a large list of items
+import React, { useState } from 'react'
+
+const Pagination = ({items,itemsPerPage}) => {
+
+    const [currentPage,setCurrentPage]=useState(); 
+
+    const indexOfLastItem = currentPage*itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = items.slice(indexOfFirstItem,indexOfLastItem);
+
+    const pageNumbers = [];
+    for(let i=1;i<Math.ceil(items.length/itemsPerPage);i++){
+        pageNumbers.push(i)
+    }
+
+  return (
+    <div>
+        <ul>
+            {currentItems.map((item,index)=>{
+                return <li key ={index}>{item}</li>
+            })}
+        </ul>
+        {pageNumbers.map((number)=>(
+            <li key={number} onClick={()=> setCurrentPage(number)}>{number}</li>
+        )
+        )}
+    </div>
+  )
+}
+
+export default Pagination
