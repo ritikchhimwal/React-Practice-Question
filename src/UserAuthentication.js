1762c20d95826780522b1a89a44dc463225455e0
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-05-11 22:32:13 +0530

    Update UserAuthentication.js

--- src/UserAuthentication.js
+++ src/UserAuthentication.js
@@ -1,1 +1,12 @@
-//
+// create a user authentication system using useContext to manage user login and logout status
+
+import React from 'react';
+
+export default function Auth(){
+  return (
+    <div>
+      <h1>User Auth</h1>
+    </div>
+  )
+}
+
